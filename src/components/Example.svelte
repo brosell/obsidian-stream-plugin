f286758b21137404f2e83e6bfd7f3721fbfe16e6
brosell <bert@rosell.net>
2023-11-12 14:26:05 -0500

    two column layout

--- src/components/Example.svelte
+++ src/components/Example.svelte
@@ -1,53 +1,75 @@
-<script lang="ts">
-  import { onMount } from "svelte";
+<script>
+  import { onMount } from 'svelte';
+  import { writable } from 'svelte/store';
 
-  let input: string = "";
+  const markdownContent = writable("# Markdown Title\n\nSome content here...");
+  let textAreaContent = "";
+  /**
+   * @type {Element}
+   */
+  let secondColumn;
+
+  // Adjust textarea height based on content until it reaches 1/3 of the window height
+  /**
+   * @param {EventTarget | null} target
+   */
+  function adjustTextareaHeight(target) {
+    if (!target) throw new Error('blarg');
+    const maxHeight = window.innerHeight / 3;
+    target.style.height = '0px'; // Reset height to recalculate
+    const newHeight = Math.min(target.scrollHeight, maxHeight);
+    target.style.height = `${newHeight}px`;
+  }
+
   onMount(() => {
-    console.log('mounted', window);
-  });
+    const resizeObserver = new ResizeObserver(entries => {
+      for (let entry of entries) {
+        // Assuming textarea is the first child of the observed element
+        adjustTextareaHeight(entry.target.firstChild);
+      }
+    });
 
+    if (secondColumn) {
+      resizeObserver.observe(secondColumn);
+    }
+
+    return () => {
+      resizeObserver.disconnect();
+    };
+  });
 </script>
 
+<div class="flex h-full">
+  <!-- First Column: Tree View -->
+  <div class="w-1/3 bg-gray-200 p-4 overflow-auto">
+    <!-- Tree view content goes here -->
+    <p>Tree View</p>
+  </div>
+
+  <!-- Second Column -->
+  <div bind:this={secondColumn} class="flex flex-col w-2/3 min-w-[300px] bg-gray-100">
+    <!-- Markdown Document -->
+    <div class="flex-1 overflow-auto p-4">
+      <p>Markdown Document</p>
+      <!-- Render markdown content here -->
+      {textAreaContent}
+    </div>
+
+    <!-- Textarea -->
+    <textarea
+      class="w-full p-2 border-t border-gray-300 resize-none"
+      placeholder="Enter your text..."
+      bind:value={textAreaContent}
+      on:input={event => adjustTextareaHeight(event.target)}
+      style="max-height: 33%;"></textarea>
+  </div>
+</div>
+
 <style>
+  /* Additional styles if necessary */
   :host {
     display: block;
     height: calc(100vh - 30);
     overflow: hidden; /* Prevents scrolling outside the component */
   }
 </style>
-
-<div class="flex flex-col h-full">
-  <div class="flex-grow overflow-auto">
-    <p>
-      Hey man, look at Marvin's hand. He can't play with his hands like that, and we can't play without him. Nah, I just don't think I'm cut out for music. C'mon. Who is that guy. Yeah, well uh, lets keep this brain melting stuff to ourselves, okay?
-    </p>
-    <p>
-      I'm sure that in 1985, plutonium is available at every corner drug store, but in 1955, it's a little hard to come by. Marty, I'm sorry, but I'm afraid you're stuck here. I can't believe you loaned me a car, without telling me it had a blindspot. I could've been killed. Don't say a word. I need fuel. Go ahead, quick, get in the car. Leave her alone, you bastard.
-    </p>
-    <p>
-      I hope so. This is it. This is the answer. It says here that a bolt of lightning is gonna strike the clock tower precisely at 10:04 p.m. next Saturday night. If we could somehow harness this bolt of lightning, channel it into the flux capacitor, it just might work. Next Saturday night, we're sending you back to the future. Where the hell are they. Yeah well, you shouldn't drink. That's Strickland. Jesus, didn't that guy ever have hair?
-    </p>
-    <p>
-      I'm sure that in 1985, plutonium is available at every corner drug store, but in 1955, it's a little hard to come by. Marty, I'm sorry, but I'm afraid you're stuck here. I can't believe you loaned me a car, without telling me it had a blindspot. I could've been killed. Don't say a word. I need fuel. Go ahead, quick, get in the car. Leave her alone, you bastard.
-    </p>
-    <p>
-      I hope so. This is it. This is the answer. It says here that a bolt of lightning is gonna strike the clock tower precisely at 10:04 p.m. next Saturday night. If we could somehow harness this bolt of lightning, channel it into the flux capacitor, it just might work. Next Saturday night, we're sending you back to the future. Where the hell are they. Yeah well, you shouldn't drink. That's Strickland. Jesus, didn't that guy ever have hair?
-    </p>
-
-    <div class="break-words">
-      <h1>Example</h1>
-      <p>Input: {input}</p>
-    </div>    
-  </div>
-
-  <div class="bg-blue-400 p-0">
-    <!-- This element will always stick to the bottom -->
-    <input
-      type="text"
-      placeholder="Write here"
-      maxlength="115"
-      bind:value={input}
-    />
-    <p>I'll just leave this here</p>
-  </div>
-</div>
