f6c722c2b8adc702b4a4bbcd7d7bde33b67056a3
brosell <bert@rosell.net>
2023-11-13 21:31:27 -0500

    rename the root component

---
+++ src/components/PluginRoot.svelte
@@ -0,0 +1,98 @@
+<script lang="ts">
+  import TreeView from './TreeView.svelte';
+  import MarkdownView from './MarkdownView.svelte';
+  import ChatInput from './ChatInput.svelte';
+  import { onMount } from 'svelte';
+
+  import { transformToTree, type TreeData } from '../services/nested-list-builder';
+  import {hierarchicalEntries} from '../testdata/tree';
+
+  let tree_data: TreeData = [];
+
+  const trans = hierarchicalEntries.map(i => ({
+      id: i.id,
+      parent: i.parent,
+      name: i.name,
+      children: [] as TreeData
+  } ));    
+    
+    const treeRaw = transformToTree(trans , i => i.parent, i => i.name);
+    tree_data = treeRaw;
+  
+  
+  let textAreaContent: string = "";
+  let chatInputComponent: ChatInput;
+
+  function adjustTextareaHeight(): void {
+    const minHeight = 100;
+    const textAreaElement: HTMLTextAreaElement = chatInputComponent.getTextAreaElement();
+    const maxHeight: number = window.innerHeight / 3;
+    // textAreaElement.style.height = '0px'; // Reset height to recalculate
+    const newHeight: number = Math.max(
+      Math.min(textAreaElement.scrollHeight, maxHeight),
+      minHeight);
+    
+    textAreaElement.style.height = `${newHeight}px`;
+  }
+
+  
+  onMount(() => {
+    const resizeObserver: ResizeObserver = new ResizeObserver(() => {
+      adjustTextareaHeight();
+    });
+
+    return () => {
+      resizeObserver.disconnect();
+    };
+  });
+
+  let startX: number;
+  let startWidth: number;
+  let leftPanel: HTMLElement;
+
+  function initResize(event: MouseEvent) {
+    startX = event.clientX;
+    startWidth = leftPanel.offsetWidth;
+    window.addEventListener('mousemove', startResizing);
+    window.addEventListener('mouseup', stopResizing);
+  }
+
+  function startResizing(event: MouseEvent) {
+    const newWidth = startWidth + event.clientX - startX;
+    leftPanel.style.width = `${newWidth}px`;
+    leftPanel.style.minWidth = `${newWidth}px`;
+  }
+
+  function stopResizing(event: MouseEvent) {
+    window.removeEventListener('mousemove', startResizing);
+    window.removeEventListener('mouseup', stopResizing);
+  }
+  
+</script>
+
+<div class="flex h-full">
+  <div bind:this={leftPanel} class=" p-4 overflow-auto" style="width:33%;">
+    <TreeView {tree_data}/>
+  </div>
+  <div id="resizer" on:mousedown={initResize}></div>
+  <div class="flex flex-col p-4" style="flex-grow:1; min-width:300px;">
+    <MarkdownView />
+    <ChatInput bind:this={chatInputComponent} {textAreaContent} {adjustTextareaHeight} />
+  </div>
+</div>
+
+<style>
+  /* Additional styles if necessary */
+  #resizer {
+    background: #ccc;
+    width: 5px;
+    min-width: 5px;
+    cursor: ew-resize;
+  }
+  /* following allows the ChatInput to stick to the bottom of the View */
+  :host {
+    display: block;
+    height: calc(100vh - 30);
+    overflow: hidden; /* Prevents scrolling outside the component */
+  }
+</style>
