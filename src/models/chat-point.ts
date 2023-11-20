37857fa56c864b4dbb6a1602d0af77ffd4309161
brosell <bert@rosell.net>
2023-11-20 14:16:16 -0500

    demo to update items in the array and samll refactor

---
+++ src/models/chat-point.ts
@@ -0,0 +1,41 @@
+export enum ChatRole {
+  SYSTEM = 'SYSTEM',
+  USER = 'USER',
+  ASSISTANT = 'ASSISTANT'
+}
+interface Completion {
+  role: ChatRole;
+  content: string;
+}
+let g_id = 0;
+export class ChatPoint {
+  id: string;
+
+  constructor(public previousId?: string, private completions: Completion[] = []) {
+    this.id = (g_id++).toString();
+  }
+
+  setUserPrompt(content: string) {
+    if (this.completions.length) {
+      throw new Error(`Cannot add User prompt because current Completion Roles [${this.completions.map(c => c.role)}] already exists`);
+    }
+
+    this.completions.push({ role: ChatRole.USER, content });
+  }
+
+  setAssistantResponse(content: string) {
+    if (this.completions[0]?.role !== ChatRole.USER) {
+      throw new Error(`Cannot add Assistant response for current Completion Roles of [${this.completions.map(c => c.role)}]`);
+    }
+
+    this.completions.push({ role: ChatRole.SYSTEM, content });
+  }
+
+  getCompletions() {
+    const answer: any = {};
+    this.completions.forEach(c => {
+      answer[c.role] = c.content;
+    });
+    return answer;
+  }
+}
--- src/stores.ts
+++ src/stores.ts
@@ -1,44 +1,8 @@
 import { writable, derived, get } from "svelte/store";
 import {marked} from 'marked';
 
-import { bus, type Message, BusEvent } from "./services/bus";
-
-enum ChatRole {
-  SYSTEM = 'SYSTEM',
-  USER = 'USER',
-  ASSISTANT = 'ASSISTANT'
-}
-
-interface Completion {
-  role: ChatRole;
-  content: string;
-}
-
-let g_id = 0;
-
-class ChatPoint {
-  id: string;
-  
-  constructor(public previousId?: string, private completions: Completion[] = []) {
-    this.id = (g_id++).toString();
-  }
-
-  setUserPrompt(content: string) {
-    if(this.completions.length) {
-      throw new Error(`Cannot add User prompt because current Completion Roles [${this.completions.map(c => c.role)}] already exists`);
-    }
-
-    this.completions.push({ role: ChatRole.USER, content } );
-  }
-
-  setAssistantResponse(content: string) {
-    if (this.completions[0]?.role !== ChatRole.USER) {
-      throw new Error(`Cannot add Assistant response for current Completion Roles of [${this.completions.map(c => c.role)}]`)
-    }
-
-    this.completions.push({ role: ChatRole.SYSTEM, content } );
-  }
-}
+import { bus } from "./services/bus";
+import { ChatPoint, ChatRole } from "./models/chat-point";
 
 const chatPoints: ChatPoint[] = [];
 
@@ -46,10 +10,15 @@ export const activeChatPointId = writable('');
 
 const deriveThread = (leafId: string): ChatPoint[] => {
   const answer = [] as ChatPoint[];
+  if (!leafId) {
+    return answer;
+  }
+
   let node=chatPoints.find(cp => cp.id === leafId);
   if (!node) {
     throw new Error(`tried to create a thread for nonexisting leaf ${leafId}`)
   }
+
   answer.unshift(node);
   while (node.previousId) {
     node = chatPoints.find(cp => cp.id === node?.previousId);
@@ -65,8 +34,11 @@ const deriveThread = (leafId: string): ChatPoint[] => {
 export const activeChatThread = derived(activeChatPointId, deriveThread);
 export const activeChatPoint = derived(activeChatPointId, id => chatPoints.find(cp => cp.id === id));
 
+// export const markdown = derived(activeChatThread, t => '# Hello');
+
+
 export const markdown = derived(activeChatThread, t => `\`\`\`
-${JSON.stringify(t, null, 2)}
+${JSON.stringify(t.map(c => c.getCompletions()), null, 2)}
 \`\`\`
 `);
 
@@ -80,7 +52,7 @@ const addChild = (content: string) => {
   chatPoints.push(child);
   activeChatPointId.set(child.id);
   
-  setTimeout(() => {child.setAssistantResponse(`echo: ${content}`); activeChatPointId.set(child.id);}, 500);
+  setTimeout(() => {child.setAssistantResponse(`echo: ${content}`); activeChatPointId.set(''); activeChatPointId.set(child.id);}, 100);
 }
 
 const commands: Record<string, (m: Record<string, any>) => void> = {
