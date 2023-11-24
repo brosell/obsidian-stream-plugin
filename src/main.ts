cbef57f817726dabaf76b3451e04d02174db6ae1
brosell <bert@rosell.net>
2023-11-24 15:15:21 -0500

    inital attempt to have multiple open views of the stream

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Plugin } from "obsidian";
+import { MarkdownView, Plugin, TFile, WorkspaceLeaf } from "obsidian";
 import { ExampleView, VIEW_TYPE_EXAMPLE } from "./views/ExampleView";
 import "virtual:uno.css";
 
@@ -22,29 +22,115 @@ export default class ObsidianNoteConnections extends Plugin {
 	}
 
 	async onload() {
-		await this.loadSettings();
+		this.registerView(VIEW_TYPE_EXAMPLE, (leaf: WorkspaceLeaf) => new ExampleView(leaf));
 
-		this.registerView(VIEW_TYPE_EXAMPLE, (leaf) => new ExampleView(leaf));
-
-		this.addRibbonIcon("dice", "Activate view", () => {
-			this.activateView();
-		});
+		this.registerEvent(this.app.workspace.on('file-open', this.handleFileOpen));
+		(window as any).toggleMyPluginView = this.toggleView.bind(this);
 	}
+
+	handleFileOpen = async (file: TFile | null): Promise<void> => {
+		if (!file) return;
+
+		const fileContent = await this.app.vault.read(file);
+		const frontmatter = this.app.metadataCache.getFileCache(file)?.frontmatter;
+
+		if (frontmatter && frontmatter.stream === 'basic') {
+			this.openCustomView(file);
+		}
+	};
+
+	openCustomView(file: TFile) {
+		let leaf = this.app.workspace.getLeaf(false);
+		if (leaf.view.getViewType() !== VIEW_TYPE_EXAMPLE) {
+				leaf.setViewState({
+						type: VIEW_TYPE_EXAMPLE,
+						state: { file: file.path }
+				}).then(() => {
+						const view = (leaf.view as ExampleView);
+						view.setFile(file);
+				});
+		}
+}
+
+// 	openCustomView(file: TFile) {
+//     // Check if there's already a leaf with the custom view for this file
+//     let leaf = this.app.workspace.getLeavesOfType(VIEW_TYPE_EXAMPLE)
+//         .find(leaf => (leaf.view as ExampleView).file.path === file.path);
+    
+//     if (!leaf) {
+//         // Create a new leaf if one doesn't exist
+//         leaf = this.app.workspace.createLeafBySplit(this.app.workspace.activeLeaf!);
+//     }
+
+//     // Open the custom view in the leaf
+//     const view = new ExampleView(leaf, file);
+//     leaf.setViewState({
+//         type: VIEW_TYPE_EXAMPLE,
+//         state: { file: file.path }
+//     });
+//     // If needed, you can manually trigger the view to display the content
+//     view.leaf.open(view);
+// }
+
+	// openCustomView(file: TFile) {
+	// 	console.log('opening view?');
+	// 	const leaf = this.app.workspace.getLeaf(false);
+	// 	leaf.setViewState({
+	// 		type: VIEW_TYPE_EXAMPLE,
+	// 		state: { file: file.path }
+	// 	});
+
+	// 	leaf.openFile(file, { state: { view: VIEW_TYPE_EXAMPLE } });
+	// }
+
+	// toggleView(file: TFile) {
+	// 		const activeLeaf = this.app.workspace.activeLeaf!;
+	// 		if (activeLeaf.view.getViewType() === 'markdown') {
+	// 				this.openCustomView(file);
+	// 		} else {
+	// 				activeLeaf.setViewState({
+	// 						type: 'markdown',
+	// 						state: { file: file.path }
+	// 				});
+	// 		}
+	// }
+
+	toggleView(file: TFile) {
+		// Find the current active markdown view
+		const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+
+		if (markdownView) {
+			// If we're in the markdown view, switch to the custom view
+			this.openCustomView(file);
+		} else {
+			// Otherwise, we're in the custom view, so switch back to the markdown view
+			const leaf = this.app.workspace.getLeaf(true);
+			leaf.setViewState({
+				type: 'markdown',
+				state: { file: file.path },
+				// You can also include other state properties if needed
+			});
+			// You may need to explicitly open the file if the view state change is not enough
+			leaf.openFile(file);
+		}
+	}
+
 
 	onunload() {
 		console.log("unloading plugin");
+		delete (window as any).toggleMyPluginView;
 	}
 
-	async activateView() {
-		this.app.workspace.detachLeavesOfType(VIEW_TYPE_EXAMPLE);
+	// async activateView() {
+	// 	this.app.workspace.detachLeavesOfType(VIEW_TYPE_EXAMPLE);
 
-		await this.app.workspace.getRightLeaf(false).setViewState({
-			type: VIEW_TYPE_EXAMPLE,
-			active: true,
-		});
+	// 	await this.app.workspace.getRightLeaf(false).setViewState({
+	// 		type: VIEW_TYPE_EXAMPLE,
+	// 		active: true,
+	// 	});
 
-		this.app.workspace.revealLeaf(
-			this.app.workspace.getLeavesOfType(VIEW_TYPE_EXAMPLE)[0],
-		);
-	}
+	// 	this.app.workspace.revealLeaf(
+	// 		this.app.workspace.getLeavesOfType(VIEW_TYPE_EXAMPLE)[0],
+	// 	);
+	// }
 }
--- src/views/ExampleView.ts
+++ src/views/ExampleView.ts
@@ -1,4 +1,4 @@
-import { ItemView } from "obsidian";
+import { ItemView, TFile, WorkspaceLeaf } from "obsidian";
 
 import Component from "../components/PluginRoot.svelte";
 
@@ -6,13 +6,26 @@ export const VIEW_TYPE_EXAMPLE = "example-view";
 
 export class ExampleView extends ItemView {
 	component!: Component;
+	file: TFile | null = null;
+
+	constructor(leaf: WorkspaceLeaf) {
+		super(leaf);
+	}
+
+	setFile(file: TFile) {
+		this.file = file;
+		console.log('file', file)
+		// Perform any additional setup required for displaying the file
+	}
 
 	getViewType() {
 		return VIEW_TYPE_EXAMPLE;
 	}
 
 	getDisplayText() {
-		return "Example view";
+		const title = this.file?.basename || 'wtf';
+		console.log('title', title);
+		return title;
 	}
 
 	async onOpen() {
